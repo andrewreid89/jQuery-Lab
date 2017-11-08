0d2fa6814a03d2e03989497d2939ec9b9ef6719b
andrewreid89 <32715271+andrewreid89@users.noreply.github.com>
2017-11-07 20:22:58 -0500

    Push while the dropdown thing works

--- script.js
+++ script.js
@@ -1,46 +1,125 @@
 $(document).ready(function(){
  
 
-
+/*hide form when user clicks X in form - this was done, but I have no idea how or why*/
     
-//show form when available is clicked//
-
-    
-    
-    
-//switch from available to reserved class//
-
-$(".Circle").click(function(){
-    $(this).addClass("reserved");
+/*when mouse enters an available seat, fade seat out (maybe 0.50 opacity)*/    
+$("button").on("mouseenter", function(){
+    if($(this).hasClass("available")){
+        $(this).fadeTo(500, 0.5);
+}}).on("mouseleave", function(){
+    $(this).fadeTo(500, 1.0);
 });
+
+
     
-
-    
-//when mouse enters an available seat, fade seat out (maybe 0.50 opacity)//    
-
-$(".Circle").hover(function(){
-    $(this).fadeTo(500,0.5);
-}, function(){
-    $(this).fadeTo(500,1.0);
-});
-    
-
-
-//alert "not allowed" if tried to select a reserved seat//
-//This doesn't work, but I don't understand why.  I tried doing just a regular alert without $(this), and it also doesn't work??
-$(".reserved").click(function(){
-    $(this).alert("This seat is already reserved");
-})
+/*Changes the a Reserved circle from red to black, and disables the clicking*/  
+$("button").on("mouseenter", function() {
+    if ($(this).hasClass("reserved")) {
+        $(this).css("background-color", "black");
+        container.off("click");
+    }});
+$("button").on("mouseenter", function(e){
+    if($(this).hasClass("reserved")){    
+        $(".HoverBox").show(500);
+        $(".HoverBox").offset({left:e.pageX,top:e.pageY});
+    }});
+$("button").on("mouseleave", function(){
+    if($(this).hasClass("reserved")){
+        $(this).css("background-color", "red");
+        $(".HoverBox").hide();
+    }});
     
  
+  
+/*Code from Mary Clare about the hover thing*/
+//$("button").on("mouseenter", function(){
+//    if($(this).hasClass
+//    
+//    
+//    click(function(e){
+//         
+// })
+//        
+    
+    
+var TableNo;    
+var container;
 
-//submitting form hides form and changes available to reserved//
+$(".available").on("click", function (){
+    
+/*show form when available is clicked*/    
+    document.querySelectorAll("#FormHolder")[0].style.display = "flex";
+    
+/*Capturing the TableNo variable in the form so I can use it later*/
+    TableNo = $(this).val();
+    
+/*Sending TableNo out to the HTML form*/    
+    var TableNoOut = document.getElementById("TableNo");
+        TableNoOut.innerText = TableNo;
+    
+/*Identifying the container as the box that is clicked*/
+    container = $(this);
+});
+
+
+    
+/*Changing class from available to reserved after clicking Save button*/    
+$("#saveBtn").on("click", function(event){
+    container.addClass("reserved");
+    container.removeClass("available");
+    $("#FormHolder").hide();
+});
+
     
 
-//change cursor hover to "not-allowed" if a seat is reserved//
-        
+//    
+//    
+///*Displaying the HoverBox on mouseenter*/
+//$(".available").on("mouseenter", function(){
+//    document.querySelectorAll(".HoverBox")[0].style.display = "block";
+//}).on("mouseleave", function(){
+//    document.querySelectorAll(".HoverBox")[0].style.display = "none";
+//}); 
+//    
+//    
     
-//hide form when user clicks X in form//
+    
+    
+    
+    
+/*Capture form inputs into variables*/
+
+//    
+//var userName;
+//var phoneNum;
+//var partyNum;
+//    
+//function formCapture(){
+//    userName = document.getElementById("Name").value;
+//    console.log(userName);
+//    
+//}    
+    
+    
+//    <input type="text" id="Name" placeholder="Name">
+//            <input type="text" id="PhoneNum" placeholder="Phone Number">
+//            <input type="text" id="NumPartyInput" placeholder="Number in your party">
+    
+    
+    
+    
+  
+    
+
+
+    
+    
+    
+    
+
+    
+
     
 
 
